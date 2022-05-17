d6c43f67cb879ee38cfd27fd9142fa85b635f40a
Omur Can Boz <omur.boz@i2i-systems.com>
2022-05-17 12:29:07 -0500

    *stripe payment has been added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
+
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Dashboard from './containers/Dashboard';
+import { config } from './Constants'
 
 function App() {
+
+  const keycloak = new Keycloak({
+    url: `${config.url.KEYCLOAK_BASE_URL}`,
+    realm: "property-realm",
+    clientId: "react-web-app"
+  })
+
+  const initOptions = { pkceMethod: 'S256' }
+
+  const handleOnEvent = async (event, error) => {
+
+    if (event === 'onAuthSuccess') {
+
+      if (keycloak.authenticated) {
+
+        
+      }
+
+    }
+
+  }
+
+
   return (
     <div className="App">
       <BrowserRouter>
-      <h1>Property Management System</h1>
       <Dashboard></Dashboard>
       </BrowserRouter>
     </div>
